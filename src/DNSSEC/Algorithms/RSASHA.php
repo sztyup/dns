b6d4e84a42b21d04937c75185d4262f97f4f6e91
Csupity László <opensource@sztyup.com>
2022-01-03 11:30:17 +0100

    Simplify algorithm implementations

--- src/DNSSEC/Algorithms/RSASHA.php
+++ src/DNSSEC/Algorithms/RSASHA.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Sztyup\Dns\DNSSEC\Algorithms;
 
+use phpseclib3\Crypt\Common\PublicKey;
+use phpseclib3\Crypt\RSA;
 use phpseclib3\Math\BigInteger;
+use RuntimeException;
 use Sztyup\Dns\Utilities\StringStream;
 
 abstract class RSASHA implements Algorithm
@@ -26,4 +29,20 @@ abstract class RSASHA implements Algorithm
         $this->modulus  = new BigInteger($stream->read($length - $stream->tell() - $start), 256);
     }
 
+    public function toPublicKey(): PublicKey
+    {
+        $key = RSA::loadParameters([
+            'modulus'  => $this->modulus,
+            'exponent' => $this->exponent,
+        ]);
+
+        if (!$key instanceof RSA || !$key instanceof PublicKey) {
+            throw new RuntimeException('Cannot create RSA key');
+        }
+
+        return $key->withHash($this->getHash())
+            ->withPadding(RSA::SIGNATURE_PKCS1);
+    }
+
+    abstract protected function getHash(): string;
 }
--- src/DNSSEC/Algorithms/RSASHA256.php
+++ src/DNSSEC/Algorithms/RSASHA256.php
@@ -4,29 +4,15 @@ declare(strict_types=1);
 
 namespace Sztyup\Dns\DNSSEC\Algorithms;
 
-use phpseclib3\Crypt\Common\PublicKey;
-use phpseclib3\Crypt\RSA;
-use RuntimeException;
-
 class RSASHA256 extends RSASHA
 {
-    public function toPublicKey(): PublicKey
-    {
-        $key = RSA::loadParameters([
-            'modulus'  => $this->modulus,
-            'exponent' => $this->exponent,
-        ]);
-
-        if (!$key instanceof RSA) {
-            throw new RuntimeException('Cannot create RSA key');
-        }
-
-        return $key->withHash('sha256')
-            ->withPadding(RSA::SIGNATURE_PKCS1);
-    }
-
     public static function getID(): int
     {
         return 8;
+    }
+
+    protected function getHash(): string
+    {
+        return 'sha256';
     }
 }
